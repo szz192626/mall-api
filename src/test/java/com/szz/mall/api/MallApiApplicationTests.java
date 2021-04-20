ac12dae33765eef8ba7f416346858cbec25bc22f
szz <1135304930@qq.com>
2021-04-20 10:34:06 +0800

    楼楼商城api项目初始化

---
+++ src/test/java/com/szz/mall/api/MallApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szz.mall.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
